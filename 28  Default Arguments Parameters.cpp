724b53e031ad4efa410ad20a1866e7c6ba7d1fd8
Zain Ahmed <48980361+zainahmed0597@users.noreply.github.com>
2020-01-30 15:50:29 +0500

    first commit

---
+++ 28  Default Arguments Parameters.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int volume(int l = 1, int w = 1, int h = 1);
+
+int main(){
+	 
+	 cout << volume();
+}
+
+int volume(int l, int w, int h){
+	
+	return l * w * h;
+}
